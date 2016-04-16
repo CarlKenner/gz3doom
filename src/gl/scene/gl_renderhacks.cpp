7fc22f118a91592e2eba0a1fda121423660fe5d1
Christopher Bruns <cmbruns@rotatingpenguin.com>
2016-04-15 21:33:33 -0400

    Add "validcount++" to get correct rendering of textures in second eye view in stereo 3D.

--- src/gl/scene/gl_renderhacks.cpp
+++ src/gl/scene/gl_renderhacks.cpp
@@ -699,6 +699,9 @@ void FDrawInfo::DrawUnhandledMissingTextures()
 
 		if (!glset.notexturefill) FloodLowerGap(seg);
 	}
+
+	validcount++; // [CMB] Allows correct rendering of textures in second eye stereoscopic view
+
 	MissingUpperTextures.Clear();
 	MissingLowerTextures.Clear();
 	MissingUpperSegs.Clear();
